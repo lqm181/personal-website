52b1897685ea72a23c053bee1e1487979b99053b
Minh Q. Le <77988736+lqm181@users.noreply.github.com>
2024-01-14 19:18:33 -0500

    Project section

--- src/sections/HeroSection.tsx
+++ src/sections/HeroSection.tsx
@@ -3,7 +3,7 @@ import { ArrowLongDownIcon, GlobeAltIcon } from '@heroicons/react/24/outline';
 
 function HeroSection() {
   return (
-    <section className='flex flex-col'>
+    <section className='flex flex-col h-screen'>
       <div className='grid grid-cols-1  lg:grid-cols-2 gap-4 h-full px-20'>
         {/* Left Section */}
         <div className='flex h-full flex-col justify-center'>
@@ -31,15 +31,17 @@ function HeroSection() {
           </p>
 
           <div className='flex justify-around w-full'>
+            <a href='#projects'>
+              <button
+                type='button'
+                className='text-white bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 min-w-32'
+              >
+                View Projects
+              </button>
+            </a>
             <button
               type='button'
-              className='text-white bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 min-w-32'
-            >
-              View Projects
-            </button>
-            <button
-              type='button'
-              className='py-2.5 px-5 me-2 mb-2 text-sm font-medium text-blue-800 focus:outline-none bg-white rounded-full border border-blue-500 hover:bg-gray-100 hover:boder-blue-700 focus:ring-4 focus:ring-blue-300 focus:z-10 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 min-w-32'
+              className='py-2.5 px-5 text-sm font-medium text-blue-800 focus:outline-none bg-white rounded-full border border-blue-500 hover:bg-gray-100 hover:boder-blue-700 focus:ring-4 focus:ring-blue-300 focus:z-10 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 min-w-32'
             >
               Connect
             </button>
