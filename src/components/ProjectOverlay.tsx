120f6c2d4f2328dff367ec921bb43e2902159122
Minh Q. Le <77988736+lqm181@users.noreply.github.com>
2024-01-18 16:02:01 -0500

    Overlay for project card whn hover

---
+++ src/components/ProjectOverlay.tsx
@@ -0,0 +1,41 @@
+import React, { HTMLAttributes } from 'react';
+
+interface ProjectOverlayProps {
+  children: React.ReactNode;
+  title: string;
+  technologies: string[];
+  technologyClassname?: HTMLAttributes<HTMLDivElement>['className'];
+}
+
+function ProjectOverlay({
+  children,
+  title,
+  technologies,
+  technologyClassname,
+}: ProjectOverlayProps) {
+  return (
+    <div className='group relative overflow-hidden container rounded-3xl text-black dark:text-white'>
+      <div className='container group-hover:opacity-50'>{children}</div>
+      <div className='flex flex-col absolute h-full w-full bg-white/85 dark:bg-black/85 -bottom-20 group-hover:bottom-0 opacity-0 group-hover:opacity-100 transition-all duration-500 justify-end p-8 lg:p-12'>
+        <h1 className='text-4xl font-bold text-black dark:text-white'>
+          {title}
+        </h1>
+        <div
+          className={`flex flex-container flex-wrap ${technologyClassname} mt-6`}
+        >
+          {technologies.map((tech, index) => (
+            <div className='chip text-black dark:text-white' key={index}>
+              {tech}
+            </div>
+          ))}
+        </div>
+
+        <h3 className='mt-8 text-xl font-semibold text-dark dark:text-white'>
+          <u>Case Study: Coming soon.</u>
+        </h3>
+      </div>
+    </div>
+  );
+}
+
+export default ProjectOverlay;
--- src/sections/ProjectsSection.tsx
+++ src/sections/ProjectsSection.tsx
@@ -1,3 +1,4 @@
+import ProjectOverlay from '@/components/ProjectOverlay';
 import Section from '@/components/Section';
 import { ComputerDesktopIcon } from '@heroicons/react/24/outline';
 import Image from 'next/image';
@@ -21,119 +22,128 @@ function ProjectsSection() {
 
       <div className='flex flex-col w-full py-4 px-2 lg:px-16 mt-4'>
         {/* Ravis */}
-        <div
-          className='flex flex-col w-full h-max min-h-[500px] rounded-3xl shadow-inner items-center py-4'
-          style={{ backgroundColor: '#A3A5FF' }}
+        <ProjectOverlay
+          title='Fullstack Software Development'
+          technologies={[
+            'Web Development',
+            'Recommender System',
+            'Nextjs',
+            'Flask',
+            'JavaScript',
+            'Neo4j',
+            'Python',
+            'React',
+            'Apache Airflow',
+            'Selenium',
+            'spaCy',
+          ]}
         >
-          <h2 className='dark:text-white font-bold text-3xl lg:text-4xl mb-2'>
-            RAVIS
-          </h2>
-          <h3 className='text-2xl lg:text-3xl font-medium'>
-            Enhance your skills with
-          </h3>
-          <h3 className='text-2xl lg:text-3xl text-center font-medium'>
-            personalized learning recommendations.
-          </h3>
-
-          {/* Ravis Technology */}
-          <div className='flex container justify-center mt-6 flex-wrap'>
-            <div className='chip'>Web Development</div>
-            <div className='chip'>Apache Airflow</div>
-            <div className='chip'>Flask</div>
-            <div className='chip'>JavaScript</div>
-            <div className='chip'>Neo4j</div>
-            <div className='chip'>Python</div>
-            <div className='chip'>React</div>
-            <div className='chip'>Selenium</div>
-            <div className='chip'>spaCy</div>
+          <div
+            className='flex flex-col w-full h-max min-h-[500px] rounded-3xl shadow-inner items-center py-4'
+            style={{ backgroundColor: '#A3A5FF' }}
+          >
+            <h2 className='text-black dark:text-white font-bold text-3xl lg:text-4xl mb-2'>
+              RAVIS
+            </h2>
+            <h3 className='text-black dark:text-white text-2xl lg:text-3xl font-medium'>
+              Enhance your skills with
+            </h3>
+            <h3 className='text-black dark:text-white text-2xl lg:text-3xl text-center font-medium'>
+              personalized learning recommendations.
+            </h3>
+            <Image
+              className='scale-75 lg:scale-75 -mt-2 -mb-12'
+              src='/assets/ravis_photo.png'
+              width={916}
+              height={1500}
+              quality={100}
+              alt='Ravis Demo Image'
+            />
           </div>
-          <Image
-            className='scale-75 lg:scale-75 -mt-2 -mb-12'
-            src='/assets/ravis_photo.png'
-            width={916}
-            height={1500}
-            quality={100}
-            alt='Ravis Demo Image'
-          />
-        </div>
+        </ProjectOverlay>
 
         <div className='grid grid-cols-1 lg:grid-cols-2 gap-8 w-full h-max mt-8'>
           {/* Epik */}
-          <div
-            className='grid grid-cols-1 lg:grid-cols-2 rounded-3xl shadow-inner'
-            style={{ backgroundColor: '#FF971E' }}
+          <ProjectOverlay
+            title='Software Development with Machine Learning'
+            technologies={[
+              'COSMIC',
+              'GPT 4.0',
+              'Langchain',
+              'Huggingface',
+              'Gradio',
+              'NLTK',
+              'PyTorch',
+              'Python',
+              'Tensorflow',
+              'spaCy',
+            ]}
           >
-            {/* Epik Title */}
-            <div className='flex flex-col flex-1 justify-center text-center lg:text-left h-full px-4 py-4 lg:pl-12 lg:py-12'>
-              <h2 className='dark:text-white font-bold text-3xl lg:text-4xl mb-12'>
-                EPIK Project
-              </h2>
-              <h3 className='text-2xl lg:text-3xl font-medium'>
-                Mitigating sexual exploitation with the power of AI.
-              </h3>
+            <div
+              className='grid grid-cols-1 lg:grid-cols-2 rounded-3xl shadow-inner min-h-[500px]'
+              style={{ backgroundColor: '#FF971E' }}
+            >
+              {/* Epik Title */}
+              <div className='flex flex-col flex-1 justify-evenly text-center lg:text-left h-full px-4 py-4 lg:pl-12 lg:py-12'>
+                <h2 className='text-black dark:text-white font-bold text-3xl lg:text-4xl mb-12'>
+                  EPIK Project
+                </h2>
+                <h3 className='text-black dark:text-white text-2xl lg:text-3xl font-medium'>
+                  Mitigating sexual exploitation with the power of AI.
+                </h3>
+              </div>
 
-              {/* Epik Technology */}
-              <div className='flex container justify-center mt-12 flex-wrap'>
-                <div className='chip'>Machine Learning</div>
-                <div className='chip'>GPT 4.0</div>
-                <div className='chip'>Huggingface</div>
-                <div className='chip'>Gradio</div>
-                <div className='chip'>NLTK</div>
-                <div className='chip'>PyTorch</div>
-                <div className='chip'>Python</div>
-                <div className='chip'>Tensorflow</div>
-                <div className='chip'>spaCy</div>
+              {/* Epik Image */}
+              <div className='flex-1 h-full min-h-72 rounded-br-3xl lg:rounded-e-3xl overflow-hidden relative'>
+                <Image
+                  className='lg:scale-125 absolute left-1/3 lg:-rotate-12'
+                  src='/assets/epik_photo.png'
+                  width={828}
+                  height={1149}
+                  quality={100}
+                  alt='Ravis Demo Image'
+                />
               </div>
             </div>
-
-            {/* Epik Image */}
-            <div className='flex-1 h-full min-h-72 rounded-br-3xl lg:rounded-e-3xl overflow-hidden relative'>
-              <Image
-                className='lg:scale-125 absolute left-1/3 lg:-rotate-12'
-                src='/assets/epik_photo.png'
-                width={828}
-                height={1149}
-                quality={100}
-                alt='Ravis Demo Image'
-              />
-            </div>
-          </div>
+          </ProjectOverlay>
 
           {/* OS */}
-          <div
-            className='grid grid-cols-1 lg:grid-cols-2 rounded-3xl shadow-inner w-full items-stretch h-full'
-            style={{ backgroundColor: '#BAE6FD' }}
+          <ProjectOverlay
+            title='Operating System Development'
+            technologies={[
+              'OS Dev',
+              'SATA',
+              'AHCI',
+              'C',
+              'Assembly x86',
+              'Linux',
+            ]}
           >
-            <div className='flex flex-col flex-1 justfiy-center text-center lg:text-left h-full px-4 py-4 lg:pl-12 lg:py-12'>
-              <h2 className='dark:text-white font-bold text-3xl lg:text-4xl mb-12'>
-                LeRimOS
-              </h2>
-              <h3 className='text-2xl lg:text-3xl font-medium'>
-                A Unix-like OS supporting SATA filesystem, task scheduling, and
-                multithreading.
-              </h3>
-
-              {/* OS Technology */}
-              <div className='flex container justify-center mt-12 flex-wrap'>
-                <div className='chip'>Operating System</div>
-                <div className='chip'>SATA</div>
-                <div className='chip'>AHCI</div>
-                <div className='chip'>C</div>
-                <div className='chip'>Assembly x86</div>
-                <div className='chip'>Linux</div>
+            <div
+              className='grid grid-cols-1 lg:grid-cols-2 rounded-3xl shadow-inner w-full items-stretch h-full min-h-[500px]'
+              style={{ backgroundColor: '#BAE6FD' }}
+            >
+              <div className='flex flex-col flex-1 justify-evenly text-center lg:text-left h-full px-4 py-4 lg:pl-12 lg:py-12'>
+                <h2 className='text-black dark:text-white font-bold text-3xl lg:text-4xl mb-12 '>
+                  LeRimOS
+                </h2>
+                <h3 className='text-black dark:text-white text-2xl lg:text-3xl font-medium'>
+                  A Unix-like OS supporting SATA filesystem, task scheduling,
+                  and multithreading.
+                </h3>
+              </div>
+              <div className='flex-1 h-full rounded-br-3xl min-h-72 overflow-hidden relative'>
+                <Image
+                  className='lg:scale-125 absolute left-1/4  lg:translate-y-1/2'
+                  src='/assets/os_photo.png'
+                  width={828}
+                  height={1149}
+                  quality={100}
+                  alt='Ravis Demo Image'
+                />
               </div>
             </div>
-            <div className='flex-1 h-full rounded-br-3xl min-h-72 overflow-hidden relative'>
-              <Image
-                className='lg:scale-125 absolute left-1/4  lg:translate-y-1/2'
-                src='/assets/os_photo.png'
-                width={828}
-                height={1149}
-                quality={100}
-                alt='Ravis Demo Image'
-              />
-            </div>
-          </div>
+          </ProjectOverlay>
         </div>
       </div>
     </Section>
