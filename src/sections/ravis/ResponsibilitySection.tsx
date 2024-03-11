84c43bb0ca7f3085b5f70eac39795a3ec450f263
Minh Q. Le <77988736+lqm181@users.noreply.github.com>
2024-03-11 03:10:01 -0400

    Icon layout for mobile

--- src/sections/ravis/ResponsibilitySection.tsx
+++ src/sections/ravis/ResponsibilitySection.tsx
@@ -16,7 +16,7 @@ const TitleIcon = ({
 }) => {
   return (
     <div
-      className={`inline-flex text-2xl md:text-4xl p-3 rounded-xl ${containerProps}`}
+      className={`w-fit h-fit text-3xl md:text-4xl p-3 rounded-xl ${containerProps}`}
     >
       {icon}
     </div>
@@ -24,69 +24,51 @@ const TitleIcon = ({
 };
 
 function ResponsibilitySection() {
+  const responsibilities = [
+    {
+      title: 'Front-end Development',
+      description:
+        'I played the pivotal role in designing and developing the front-end of RAVIS using Next.js and React.',
+      icon: <BsWindowSidebar className='stroke-[0.25]' />,
+    },
+    {
+      title: 'OAuth with Azure',
+      description:
+        'I was in charge of setting up the authentication logic like sign in/sign up, page protection, etc. for the application using NextAuth and Microsoft Azure.',
+      icon: <BsShieldCheck className='stroke-[0.25]' />,
+    },
+    {
+      title: 'Search System with Pagination',
+      description:
+        "I developed the website's search system, enabling users to search courses using keywords and sorting/filtering options. Additionally, I implemented server-side pagination for the search page to optimize bandwidth and enhance system performance.",
+      icon: <BsSearch className='stroke-[0.25]' />,
+    },
+    {
+      title: 'Data Access Layer Design',
+      description:
+        'I designed the data access layer to connect our Flask back-end with Neo4j database. This creates a separation of logic from our API layer which enables reusability, scalability, and great maintainability for the system.',
+      icon: <BsDatabase className='stroke-[0.25]' />,
+    },
+  ];
+
   return (
     <div className='text-sm md:text-base'>
       <h1 className='project-section-title mb-4'>My Responsibilities</h1>
       <div className='grid grid-cols-1 md:grid-cols-2 gap-8'>
-        <div>
-          <TitleIcon
-            icon={<BsWindowSidebar className='stroke-[0.25]' />}
-            containerProps='bg-emerald-100 text-emerald-600 mb-1'
-          />
-          <h2 className='project-section-subtitle mb-2'>
-            Front-end Development
-          </h2>
-          <p>
-            I played the pivotal role in designing and developing the front-end
-            of RAVIS using Next.js and React.
-          </p>
-        </div>
-
-        <div>
-          <TitleIcon
-            icon={<BsShieldCheck className='stroke-[0.25]' />}
-            containerProps='bg-emerald-100 text-emerald-600 mb-1'
-          />
-
-          <h2 className='project-section-subtitle mb-2'>OAuth with Azure</h2>
-          <p>
-            I was in charge of setting up the authentication logic like sign
-            in/sign up, page protection, etc. for the application using NextAuth
-            and Microsoft Azure.
-          </p>
-        </div>
-
-        <div>
-          <TitleIcon
-            icon={<BsSearch className='stroke-[0.25]' />}
-            containerProps='bg-emerald-100 text-emerald-600 mb-1'
-          />
-          <h2 className='project-section-subtitle mb-2'>
-            Search System with Pagination
-          </h2>
-          <p>
-            I developed the website's search system, enabling users to search
-            courses using keywords and sorting/filtering options. Additionally,
-            I implemented server-side pagination for the search page to optimize
-            bandwidth and enhance system performance.
-          </p>
-        </div>
-
-        <div>
-          <TitleIcon
-            icon={<BsDatabase className='stroke-[0.25]' />}
-            containerProps='bg-emerald-100 text-emerald-600 mb-1'
-          />
-          <h2 className='project-section-subtitle mb-2'>
-            Data Access Layer Design
-          </h2>
-          <p>
-            I designed the data access layer to connect our Flask back-end with
-            Neo4j database. This creates a separation of logic from our API
-            layer which enables reusability, scalability, and great
-            maintainability for the system.
-          </p>
-        </div>
+        {responsibilities.map((item, index) => {
+          return (
+            <div key={index} className='flex md:flex-col'>
+              <TitleIcon
+                icon={item.icon}
+                containerProps='bg-emerald-100 text-emerald-600'
+              />
+              <div className='ml-4 md:ml-0 md:mt-2'>
+                <h2 className='project-section-subtitle mb-1'>{item.title}</h2>
+                <p>{item.description}</p>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
